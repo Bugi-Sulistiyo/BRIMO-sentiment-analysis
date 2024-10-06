98ea3521332601e3501d5afb617e450c668c4e44
Bugi-Sulistiyo <71893631+Bugi-Sulistiyo@users.noreply.github.com>
2024-10-06 23:58:21 +0800

    add some documentation

--- a. scrapping.py
+++ a. scrapping.py
@@ -1,6 +1,8 @@
+# Import the necessary packages
 from google_play_scraper import search, reviews, Sort
 import csv
 
+# configure the search
 scrapped_data, _ = reviews(
     "id.co.bri.brimo",
     lang="id",
@@ -9,6 +11,7 @@ scrapped_data, _ = reviews(
     count=12000
 )
 
+# Save the data to a CSV file
 with open("brimo_reviews.csv", mode="w", newline="", encoding="utf-8") as file:
     field_names = list(scrapped_data[0].keys())
     writer = csv.DictWriter(file, fieldnames=field_names)
